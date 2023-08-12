64be21db67c2a1ea687f35175fe92b7dfdcba882
nacim <msallem.nacim@gmail.com>
2023-08-12 16:45:13 +0100

    navbar/header

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,16 @@ import {
   Route,
 }from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
+import List from "./pages/list/List.jsx";
+import Hotel from "./pages/hotel/Hotel.jsx";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotel/>}/>
+
       </Routes>
     </BrowserRouter>
   );
